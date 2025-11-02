270aeb63d65faf4e9d1f30f610f702e814c2cf3f
andrewpr52 <andrewpr52@gmail.com>
2025-11-01 23:15:08 -0500

    added checking team logs

--- main.py
+++ main.py
@@ -273,8 +273,10 @@ def main():
         if check_home_team or check_away_team:
             game = GameDetails(game_id)
             if check_home_team:
+                util.logger.info(f'{game_id} - Checking home team ({game_home_team_id})', extra={'game_id': game_id, 'game_status': game_status, 'home_team_id': game_home_team_id, 'away_team_id': game_away_team_id})
                 check_no_hitter(game, game_home_team_id)
             if check_away_team:
+                util.logger.info(f'{game_id} - Checking away team ({game_away_team_id})', extra={'game_id': game_id, 'game_status': game_status, 'home_team_id': game_home_team_id, 'away_team_id': game_away_team_id})
                 check_no_hitter(game, game_away_team_id)
 
 
